5c1bc3ef679a6e7f60084b3246aa62909bcbd471
brunogouveia <bruno.henrique.gouveia@gmail.com>
2015-09-18 14:39:23 -0400

    Now compiling with Cmake (Mac only)

---
+++ build/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Bruno/Downloads/imgui-marota/libs/gl3w/GL/gl3w.c" "/Users/Bruno/Downloads/imgui-marota/build/CMakeFiles/example.dir/libs/gl3w/GL/gl3w.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../libs/gl3w"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Bruno/Downloads/imgui-marota/example.cpp" "/Users/Bruno/Downloads/imgui-marota/build/CMakeFiles/example.dir/example.cpp.o"
+  "/Users/Bruno/Downloads/imgui-marota/src/ImGui/imgui.cpp" "/Users/Bruno/Downloads/imgui-marota/build/CMakeFiles/example.dir/src/ImGui/imgui.cpp.o"
+  "/Users/Bruno/Downloads/imgui-marota/src/ImGui/imgui_demo.cpp" "/Users/Bruno/Downloads/imgui-marota/build/CMakeFiles/example.dir/src/ImGui/imgui_demo.cpp.o"
+  "/Users/Bruno/Downloads/imgui-marota/src/ImGui/imgui_draw.cpp" "/Users/Bruno/Downloads/imgui-marota/build/CMakeFiles/example.dir/src/ImGui/imgui_draw.cpp.o"
+  "/Users/Bruno/Downloads/imgui-marota/src/ImGui/imgui_impl_glfw_gl3.cpp" "/Users/Bruno/Downloads/imgui-marota/build/CMakeFiles/example.dir/src/ImGui/imgui_impl_glfw_gl3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../libs/gl3w"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/example.cpp.o"
+  "CMakeFiles/example.dir/src/ImGui/imgui.cpp.o"
+  "CMakeFiles/example.dir/src/ImGui/imgui_demo.cpp.o"
+  "CMakeFiles/example.dir/src/ImGui/imgui_draw.cpp.o"
+  "CMakeFiles/example.dir/src/ImGui/imgui_impl_glfw_gl3.cpp.o"
+  "CMakeFiles/example.dir/libs/gl3w/GL/gl3w.c.o"
+  "example.pdb"
+  "example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
